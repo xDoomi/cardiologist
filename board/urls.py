a97ea56b256ef475702a28e6c89fa3b99fb39619
Pavel Piasetski <pyasecky2010pavel@mail.ru>
2021-11-06 22:50:01 +0300

    Change landing page & routing

--- board/urls.py
+++ board/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.MainView.as_view(), name="main"),
     path("<int:game_id>/detail/", views.DetailView.as_view(), name="detail"),
     path("detail/", views.FavouriteView.as_view(), name="edit"),
-    path("favourite/", views.GetFavouriteView.as_view(), name="favourite"),
+    path("history/", views.GetFavouriteView.as_view(), name="history"),
 ]
